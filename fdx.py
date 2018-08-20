201768711a663ca7b0568653cd561f8e53057ffe
tdrk1980 <jabberwock_takatuki@yahoo.co.jp>
2018-08-21 06:45:50 +0900

    add fdx

---
+++ fdx.py
@@ -0,0 +1,9 @@
+# *-* encoding: utf-8 *-*
+from ctypes import Structure
+
+class Fdx(Structure):
+    pass
+
+
+# C:\Users\Public\Documents\Vector\CANoe\Sample Configurations 11.0.42\IO_HIL\FDX
+# C:\Users\Public\Documents\Vector\CANoe\Sample Configurations 11.0.42\IO_HIL\FDX\EasyClient
